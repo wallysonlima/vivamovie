d39241c66d3b8cdff8287ce80a61038772824ab6
wallysonlima <wallyson.n.a.lima@gmail.com>
2021-02-06 17:28:34 -0300

    Create MainOptionsActivity

---
+++ app/src/main/java/wallyson/lima/vivamovie/view/UI_MainOptionsActivity.kt
@@ -0,0 +1,12 @@
+package wallyson.lima.vivamovie.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import wallyson.lima.vivamovie.R
+
+class UI_MainOptionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_options)
+    }
+}
--- app/src/main/java/wallyson/lima/vivamovie/view/UI_RegisterActivity.kt
+++ app/src/main/java/wallyson/lima/vivamovie/view/UI_RegisterActivity.kt
@@ -2,6 +2,7 @@ package wallyson.lima.vivamovie.view
 
 import android.app.Dialog
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -55,6 +56,8 @@ class UI_RegisterActivity : AppCompatActivity(), RegisterInterface {
     override fun toRegister() {
         if ( mPresenter.toRegister() ) {
             Toast.makeText(this, getString(R.string.successregister), Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, UI_MainOptionsActivity::class.java)
+            startActivity(intent)
         } else {
             if ( editTextName.text.isNotEmpty() && editTextPassword.text.isNotEmpty() ) {
                 Toast.makeText(this, getString(R.string.fill), Toast.LENGTH_SHORT).show()
