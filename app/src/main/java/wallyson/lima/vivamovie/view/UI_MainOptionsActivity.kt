4fa44df3da33cd252ada0d504e0057ea2c223f9b
wallysonlima <wallyson.n.a.lima@gmail.com>
2021-02-11 10:54:17 -0300

    Updated MainOptionsActivity

--- app/src/main/java/wallyson/lima/vivamovie/view/UI_MainOptionsActivity.kt
+++ app/src/main/java/wallyson/lima/vivamovie/view/UI_MainOptionsActivity.kt
@@ -7,12 +7,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_register.view.*
 import kotlinx.android.synthetic.main.quantity.view.*
 import wallyson.lima.vivamovie.R
 
 class UI_MainOptionsActivity : AppCompatActivity() {
     private lateinit var buttonListMovies: Button
+    private lateinit var buttonGenre: Button
+    private lateinit var buttonCompany: Button
+    private lateinit var buttonContact: Button
+    private lateinit var buttonLocalization: Button
+    private lateinit var buttonAbout: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +27,26 @@ class UI_MainOptionsActivity : AppCompatActivity() {
         buttonListMovies.setOnClickListener {
             listMovies()
         }
-    }
 
-    fun initiate() {
-        buttonListMovies = findViewById(R.id.buttonListMovies)
+        buttonGenre.setOnClickListener {
+            listGenre()
+        }
+
+        buttonCompany.setOnClickListener {
+            listCompanies()
+        }
+
+        buttonContact.setOnClickListener {
+            contact()
+        }
+
+        buttonLocalization.setOnClickListener {
+            localization()
+        }
+
+        buttonAbout.setOnClickListener {
+            about()
+        }
     }
 
     fun listMovies() {
@@ -44,10 +64,47 @@ class UI_MainOptionsActivity : AppCompatActivity() {
 
             if (Integer.parseInt(number) in 4..12) {
                 val intent = Intent(this, UI_MoviesActivity::class.java)
+                intent.putExtra("type", "movies")
                 startActivity(intent)
             } else {
                 Toast.makeText(this, getString(R.string.tryagain), Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun listGenre() {
+        val intent = Intent(this, UI_MoviesActivity::class.java)
+        intent.putExtra("type", "genre")
+        startActivity(intent)
+    }
+
+    private fun listCompanies() {
+        val intent = Intent(this, UI_MoviesActivity::class.java)
+        intent.putExtra("type", "company" )
+        startActivity(intent)
+    }
+
+    private fun contact() {
+        val intent = Intent(this, UI_ContactActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun localization() {
+        val intent = Intent(this, UI_LocalizationActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun about() {
+        val intent = Intent(this, UI_AboutActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun initiate() {
+        buttonListMovies = findViewById(R.id.buttonListMovies)
+        buttonGenre = findViewById(R.id.buttonGenre)
+        buttonCompany = findViewById(R.id.buttonCompany)
+        buttonContact = findViewById(R.id.buttonContact)
+        buttonLocalization = findViewById(R.id.buttonLocalization)
+        buttonAbout = findViewById(R.id.buttonAbout)
     }
 }
